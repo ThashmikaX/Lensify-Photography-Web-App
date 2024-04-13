6ce68219374279a7b3bfa537593ac22f3e43cc12
ThashmikaX <74942602+ThashmikaX@users.noreply.github.com>
2024-04-13 17:11:20 +0530

    Add react-router-dom package

---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import bg from '../assets/images/bg.jpg'
+
+const Landing = () => {
+  return (
+      <div>
+      <h1>Hello World</h1>
+      <img src={bg} className='main-bg'/>
+    </div>
+  )
+}
+
+export default Landing
