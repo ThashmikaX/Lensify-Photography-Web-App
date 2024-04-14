87becd2ec711e9144ac4f2dbf2acc1f115d67ef2
ThashmikaX <74942602+ThashmikaX@users.noreply.github.com>
2024-04-14 16:00:12 +0530

    style updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,17 @@ import './App.css'
 import { Landing } from './pages'
 import { Navbar } from './components';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import bg from './assets/images/bg.jpg'
 
 function App() {
   return (
     <Router>
       <div className="container1">
         <Navbar />
-        <Routes>
+        <Routes className="container2">
           <Route path="/" element={<Landing className='Landing' />}  />
         </Routes>
+        <img src={bg} className='main-bg'/>
       </div>
       
     </Router>
