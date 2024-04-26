1acd22d083517ff6e1096d1f42444e67baedd2b8
ThashmikaX <74942602+ThashmikaX@users.noreply.github.com>
2024-04-26 23:38:18 +0530

    Update landing page style and add scroll related packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import bg from './assets/images/bg.jpg'
 import styled from "styled-components";
 import AccountBox from "../src/pages/accountBox"
+import { Link, Element } from 'react-scroll';
 
 function App() {
   const AppContainer = styled.div`
@@ -19,12 +20,24 @@ function App() {
   return (
     <Router>
       <div className="container1">
-        <Navbar />
+        <Navbar className="nav-bar"></Navbar>
         <Routes className="container2">
-          <Route path="/" element={<Landing className='Landing' />} />
+          <Route path="/" element={
+            <>
+              {/* <Link to="section1" smooth={true}>Go to Section 1</Link>
+              <Link to="section2" smooth={true}>Go to Section 2</Link> */}
+
+              <Element name="section1" className='Landing section1'>
+                <Landing />
+                <img src={bg} className='main-bg'/>
+              </Element>
+              <Element name="section2">
+              </Element>
+            </>
+          } />
           <Route path='/login' element={<AppContainer><AccountBox /></AppContainer>} />
         </Routes>
-        <img src={bg} className='main-bg'/>
+        
       </div>
       
     </Router>
