ab1eb3b6b0151e0f354e4fa50640094cb988a875
ThashmikaX <74942602+ThashmikaX@users.noreply.github.com>
2024-04-26 22:41:34 +0530

    Update Landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,26 @@ import { Landing } from './pages'
 import { Navbar } from './components';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import bg from './assets/images/bg.jpg'
+import styled from "styled-components";
+import AccountBox from "../src/pages/accountBox"
 
 function App() {
+  const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+  
   return (
     <Router>
       <div className="container1">
         <Navbar />
         <Routes className="container2">
-          <Route path="/" element={<Landing className='Landing' />}  />
+          <Route path="/" element={<Landing className='Landing' />} />
+          <Route path='/login' element={<AppContainer><AccountBox /></AppContainer>} />
         </Routes>
         <img src={bg} className='main-bg'/>
       </div>
