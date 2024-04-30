94b2147db470a360725d020d272797d710104e71
ThashmikaX <74942602+ThashmikaX@users.noreply.github.com>
2024-04-30 20:50:51 +0530

    Add axios package and implement signup form functionality

--- client/src/pages/accountBox/signupForm.jsx
+++ client/src/pages/accountBox/signupForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import {
   BoldLink,
   BoxContainer,
@@ -10,20 +10,71 @@ import {
 } from "./common";
 import { Marginer } from "../marginer";
 import { AccountContext } from './accountContext';
+import Axios from "axios";
+
+
+
 
 export function SignupForm(props) {
 
+  const [firstName, setfirstName] = useState("");
+  const [lastName, setlastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const handleSubmit = (event) => {
+    console.log("submitting");
+    event.preventDefault();
+    console.log(firstName, lastName, email, confirmPassword);
+  Axios.post("http://localhost:3001/register", {
+    FirstName: firstName,
+    LastName: lastName,
+    Email: email,
+    Password: confirmPassword,
+  })
+  .then(response => {
+    console.log(response);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+   
+
   const { switchToSignin } = useContext(AccountContext);
   return (
-    <BoxContainer>
+    <BoxContainer onSubmit={handleSubmit}>
       <FormContainer>
-        <Input type="text" placeholder="Full name" />
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
-        <Input type="password" placeholder="Confirm password" />
+        <Input type="text" placeholder="First name"
+          onChange={(e) => {
+            setfirstName(e.target.value);
+          }} />
+        <Input type="text" placeholder="Last name"
+          onChange={(e) => {
+            setlastName(e.target.value);
+            }} />
+        <Input type="email" placeholder="Email"
+        onChange={(e) => {
+            setEmail(e.target.value);
+            }}/>
+        <Input type="password" placeholder="Password"
+        onChange={(e) => {
+            setPassword(e.target.value);
+            }}/>
+        <Input type="password" placeholder="Confirm password"
+          onChange={(e) => {
+            if (e.target.value == password) {
+              setConfirmPassword(e.target.value);
+            }
+            else {
+              console.log("Passwords do not match");   
+            }
+          }} />
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
-      <a href="/complete"><SubmitButton href="/complete" type="submit">Signup</SubmitButton></a>
+      <button type="submit" onClick={handleSubmit} className="button submit">
+          Submit
+      </button>
       <Marginer direction="vertical" margin="5px" />
       <LineText>
         Already have an account?{" "}
